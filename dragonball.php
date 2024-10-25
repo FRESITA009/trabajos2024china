96c2b6f92cb8971bc73dfc8e65bdf0a9aa3ceeda
kimberly gutierrez <gutierrezkimberly896@gmail.com>
2024-10-25 08:31:00 -0600

    Add files via upload

---
+++ dragonball.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.1
+ */
+
+/**
+ * Database `dragonball`
+ */
+
+/* `dragonball`.`characters` */
+$characters = array(
+);
+
+/* `dragonball`.`character_transformations` */
+$character_transformations = array(
+);
+
+/* `dragonball`.`tournaments` */
+$tournaments = array(
+);
+
+/* `dragonball`.`tournament_participants` */
+$tournament_participants = array(
+);
+
+/* `dragonball`.`transformations` */
+$transformations = array(
+);
+
+/* `dragonball`.`users` */
+$users = array(
+);
